35680e618d60bc94882d22051d1b72eeff024400
penghui.yang <penghui.yang@renren-inc.com>
2019-08-02 15:58:52 +0800

    rebuilding site 2019年 8月 2日 星期五 15时58分52秒 CST

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
-# Go To Public folder
-cd public
-# Add changes to git.
-git add -A
+git add .
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -20,5 +17,30 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
-# Come Back
-cd ..
+# 开始发布
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "Push to origin"
+git push origin gh-pages
